14e918636d4acb34f7a635fa4d27b80f85225cd9
Glenn Meyer <grossmeyer@gmail.com>
2021-01-08 13:41:36 -0500

    Added Rating component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
@@ -7,21 +8,27 @@ const Product = ({ product }) => {
       <a href={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top'></Card.Img>
       </a>
+
       <Card.Body>
         <a href={`/product/${product._id}`}>
           <Card.Title as='div'><strong>{product.name}</strong></Card.Title>
         </a>
+
         <Card.Text as='div'>
-          <div className='my-3'>
-            {product.rating} from {product.numReviews} reviews
-          </div>
+          <Rating value={product.rating} text={`${product.numReviews} reviews`}>
+          </Rating>
         </Card.Text>
+
         <Card.Text as='h3'>
           ${product.price}
         </Card.Text>
       </Card.Body>
     </Card>
   )
+}
+
+Rating.defaultProps = {
+  color: '#f39c12'
 }
 
 export default Product
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,7 +9,7 @@ const HomeScreen = () => {
       <h1>Latest Products</h1>
       <Row>
         {products.map(p => (
-          <Col sm={12} md={6} lg={4} xl={3}>
+          <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={p} />
           </Col>
         ))}
